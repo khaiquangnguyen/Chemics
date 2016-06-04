981074dbd980607fc2f02c7f1af8494b24d40710
khaiquangnguyen <kqn001@bucknell.edu>
2016-06-04 17:13:22 -0400

    test

---
+++ GUI.py
@@ -0,0 +1,65 @@
+import PySide
+import sys
+
+from PySide.QtGui import *
+from PySide.QtGui import *
+from PySide.QtCore import *
+
+qt_app = QApplication(sys.argv)
+
+
+class LayoutExample(QWidget):
+    ''' An example of PySide/PyQt absolute positioning; the main window
+        inherits from QWidget, a convenient widget for an empty window. '''
+
+    def __init__(self):
+        # Initialize the object as a QWidget and
+        # set its title and minimum width
+        QWidget.__init__(self)
+        self.setWindowTitle('Chemics')
+        screen = QDesktopWidget().screenGeometry()
+        self.setMinimumSize(screen.width()/2,screen.height()/2)
+
+        # addSubWidget
+        self.subWidget = subWidget()
+        self.graphWidget = graphWidget()
+        self.layout = QGridLayout()
+        self.layout.addWidget(self.subWidget,0,0,0,1)
+        self.layout.addWidget(self.graphWidget,0,1,0,3)
+        self.setLayout(self.layout)
+
+
+    def run(self):
+        # Show the form
+        self.show()
+        # Run the qt application
+        qt_app.exec_()
+
+class subWidget(QWidget):
+    def __init__(self, parent=None):
+        super(self.__class__,self).__init__(parent)
+        self.layout = QVBoxLayout()  #Vertical layout
+        self.setLayout(self.layout)
+
+        #Set color for layout for easier managing
+        self.setAutoFillBackground(True)
+        palette = QPalette()
+        palette.setColor(QPalette.Background, Qt.red)
+        self.setPalette(palette)
+
+
+class graphWidget(QWidget):
+    def __init__(self, parent=None):
+        super(self.__class__, self).__init__(parent)
+        self.layout = QVBoxLayout()  # Vertical layout
+        self.setLayout(self.layout)
+
+        # Set color for layout for easier managing
+        self.setAutoFillBackground(True)
+        palette = QPalette()
+        palette.setColor(QPalette.Background, Qt.blue)
+        self.setPalette(palette)
+
+# Create an instance of the application window and run it
+app = LayoutExample()
+app.run()
