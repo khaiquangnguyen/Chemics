b8ec245adce531da621753aa8b22a0798f3a7e29
khaiquangnguyen <kqn001@bucknell.edu>
2016-06-08 18:45:36 -0400

    Visualization and a lot of other things

--- GUI.py
+++ GUI.py
@@ -1,11 +1,13 @@
-import PySide
+
 import sys
 
-from PySide.QtGui import *
 from PySide.QtGui import *
 from PySide.QtCore import *
 
 qt_app = QApplication(sys.argv)
+
+width  = 0
+height = 0
 
 
 class MainWindow(QMainWindow):
@@ -14,9 +16,14 @@ class MainWindow(QMainWindow):
 
         # set Size
         self.setWindowTitle('Chemics')
-        screen = QDesktopWidget().screenGeometry()
-        self.setMinimumSize(screen.width() / 2, screen.height() / 2)
-
+        # screen = QDesktopWidget().screenGeometry()
+        # width = screen.width()
+        # height = screen.height()
+        global width, height
+        self.setMinimumHeight(100)
+        self.setMinimumWidth(100)
+        width = self.width()
+        height = self.height()
         #add Menu
         exitAction = QAction( '&Exit', self)
         exitAction.setShortcut('Ctrl+Q')
@@ -27,12 +34,15 @@ class MainWindow(QMainWindow):
         fileMenu.addAction(exitAction)
         self.setCentralWidget(ControlPanel())
 
+
     def run(self):
         # Show the form
         self.show()
         # Run the qt application
         qt_app.exec_()
 
+    def resizeEvent(self, resizeEvent):
+        self.centralWidget().resize()
 
 class ControlPanel(QWidget):
     ''' An example of PySide/PyQt absolute positioning; the main window
@@ -44,21 +54,21 @@ class ControlPanel(QWidget):
         QWidget.__init__(self)
 
         #remove border
-        self.setFrame
 
         # addSubWidget
         self.subWidget = infoWidget()
         self.graphWidget = graphWidget()
-        self.layout = QGridLayout()
-        self.layout.addWidget(self.subWidget,0,0,0,1)
-        self.layout.addWidget(self.graphWidget,0,1,0,3)
+        self.layout = QHBoxLayout()
+        self.layout.setSpacing(0)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.layout.addWidget(self.subWidget)
+        self.layout.addWidget(self.graphWidget)
         self.setLayout(self.layout)
 
-    def run(self):
-        # Show the form
-        self.show()
-        # Run the qt application
-        qt_app.exec_()
+    def resize(self):
+        self.subWidget.resize(self.width(),self.height())
+        self.graphWidget.resize(self.width(),self.height())
+
 
 class infoWidget(QWidget):
     def __init__(self, parent=None):
@@ -72,18 +82,93 @@ class infoWidget(QWidget):
         palette.setColor(QPalette.Background, Qt.red)
         self.setPalette(palette)
 
+    def resize(self,width, height):
+        self.setFixedWidth(width / 4 )
+        self.setFixedHeight(height)
+
 
 class graphWidget(QWidget):
+    def resize(self, width, height):
+        self.setFixedWidth(width * 3 / 4)
+        self.setFixedHeight(height)
+        self.totalView.resize(self.height())
+
     def __init__(self, parent=None):
         super(self.__class__, self).__init__(parent)
-        self.layout = QVBoxLayout()  # Vertical layout
+        self.layout = QVBoxLayout()
         self.setLayout(self.layout)
-
+        self.layout.setSpacing(0)
+        self.layout.setContentsMargins(0, 0, 0, 0)
         # Set color for layout for easier managing
         self.setAutoFillBackground(True)
         palette = QPalette()
         palette.setColor(QPalette.Background, Qt.blue)
         self.setPalette(palette)
+
+        graph = QPixmap("graph.png")
+
+        self.totalView = totalView()
+        self.individualViews = individualViews()
+        self.layout.addWidget(self.totalView)
+        self.layout.addWidget(self.individualViews)
+
+class totalView(QGraphicsView):
+    def resize(self, height):
+        self.setFixedHeight(height * 2/5)
+
+    def __init__(self, parent=None):
+        super(self.__class__, self).__init__(parent)
+        graph = QPixmap("graph.png")
+        self.totalScene = QGraphicsScene()
+        self.totalScene.addPixmap(graph)
+        self.setScene(self.totalScene)
+
+class individualViews(QWidget):
+    def __init__(self):
+        # Initialize the object as a QWidget and
+        # set its title and minimum width
+        QWidget.__init__(self)
+
+        # remove border
+        # addSubWidget
+        self.dpView = dpView()
+        self.dNlogView = dNlogView()
+        self.layout = QHBoxLayout()
+        self.layout.setSpacing(0)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.layout.addWidget(self.dpView)
+        self.layout.addWidget(self.dNlogView)
+
+        self.setLayout(self.layout)
+
+
+class dpView(QGraphicsView):
+    def resize(self, width):
+        self.setFixedWidth(width /2 )
+
+    def __init__(self, parent=None):
+        super(self.__class__, self).__init__(parent)
+        graph = QPixmap("graph.png")
+        self.totalScene = QGraphicsScene()
+        self.totalScene.addPixmap(graph)
+        self.setScene(self.totalScene)
+        self.setRenderHint(QPainter.Antialiasing or QPainter.SmoothPixmapTransform or QPainter.HighQualityAntialiasing)
+        self.fitInView(self.totalScene.itemsBoundingRect(), Qt.KeepAspectRatio)
+
+
+class dNlogView(QGraphicsView):
+    def resize(self, width):
+        self.setFixedWidth(width / 2)
+
+    def __init__(self, parent=None):
+        super(self.__class__, self).__init__(parent)
+        graph = QPixmap("graph.png")
+        self.totalScene = QGraphicsScene()
+        self.totalScene.addPixmap(graph)
+        self.setScene(self.totalScene)
+        self.setRenderHint(QPainter.Antialiasing or QPainter.SmoothPixmapTransform or QPainter.HighQualityAntialiasing)
+
+
 
 # Create an instance of the application window and run it
 app = MainWindow()
