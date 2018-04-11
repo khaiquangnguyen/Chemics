f0e8d7b341ecd0201c5da250d594dd997ddb0348
khaiquangnguyen <kqn001@bucknell.edu>
2018-04-11 12:56:04 -0400

    Merge branch 'master' of https://github.com/khaiquangnguyen/Chemics
    # Conflicts:
    #	.idea/Chemics.iml

--- Graphs.py
+++ Graphs.py
@@ -20,8 +20,7 @@ class ConcOverTimeRawDataGraph(FigureCanvas):
         self.smps_points, = self.ax.plot([], [], label="Raw SMPS")
         self.ccnc_points, = self.ax.plot([], [], label="Raw CCNC")
         handles, labels = self.ax.get_legend_handles_labels()
-        legend = self.ax.legend(handles, labels, loc=2, )
-        legend.get_frame().set_alpha(0.3)
+        self.ax.legend(handles, labels)
         self.ax.set_title("Raw SMPS and CCNC concentration over time")
 
     def update_graph(self, a_scan, smooth_ccnc=False):
@@ -56,23 +55,21 @@ class ConcOverTimeRawDataGraph(FigureCanvas):
 
 class ConcOverTimeSmoothGraph(FigureCanvas):
     def __init__(self, parent=None):
-        self.fig, self.ax = plt.subplots(facecolor=CONST.GRAPH_BACKGROUND_COLOR)
+        plt.style.use('ggplot')
+        self.fig, self.ax = plt.subplots()
+        self.fig, self.ax = plt.subplots()
         super(self.__class__, self).__init__(self.fig)
-        self.ax.set_autoscale_on(True)
         self.ax.axes.set_frame_on(False)
-        self.ax.grid(color=CONST.GRID_COLOR)
-        self.ax.axhline(0, color=CONST.AX_LINE_COLOR, linewidth=4)
-        self.ax.axvline(0, color=CONST.AX_LINE_COLOR, linewidth=4)
-        self.ax.tick_params(color=CONST.AX_TICK_COLOR, which='both', labelcolor=CONST.AX_TICK_COLOR,
-                            labelsize=CONST.AX_TICK_SIZE)
-        self.ax.set_xlabel("Scan time(s)", color=CONST.LABEL_COLOR, size=CONST.LABEL_SIZE)
-        self.ax.set_ylabel("Concentration (1/cm3)", color=CONST.LABEL_COLOR, size=CONST.LABEL_SIZE)
-        self.smps_points, = self.ax.plot([], [], linewidth=4, color=CONST.SMPS_LINE_COLOR, label="SMPS")
-        self.ccnc_points, = self.ax.plot([], [], linewidth=4, color=CONST.CCNC_LINE_COLOR, label="CCNC")
+        self.ax.axhline(0)
+        self.ax.axvline(0)
+        self.ax.tick_params(which='both')
+        self.ax.set_xlabel("Scan time(s)")
+        self.ax.set_ylabel("Concentration (1/cm3)")
+        self.smps_points, = self.ax.plot([], [], label="SMPS")
+        self.ccnc_points, = self.ax.plot([], [], label="CCNC")
         handles, labels = self.ax.get_legend_handles_labels()
-        legend = self.ax.legend(handles, labels, loc=2, fontsize=CONST.LEGEND_FONT_SIZE)
-        legend.get_frame().set_alpha(0.3)
-        self.ax.set_title("SMPS and CCNC concentration over scan time", color=CONST.TITLE_COLOR, size=CONST.TITLE_SIZE)
+        self.ax.legend(handles, labels)
+        self.ax.set_title("SMPS and CCNC concentration over scan time")
 
     def update_graph(self, a_scan):
         smps_counts = a_scan.processed_smps_counts
@@ -106,21 +103,17 @@ class RatioOverDiameterGraph(FigureCanvas):
         self.fig, self.ax = plt.subplots(facecolor=CONST.GRAPH_BACKGROUND_COLOR)
         super(self.__class__, self).__init__(self.fig)
         self.ax.axes.set_frame_on(False)
-        self.ax.grid(color='0.5')
-        self.ax.axhline(0, color=CONST.AX_LINE_COLOR, linewidth=2)
-        self.ax.axvline(0, color=CONST.AX_LINE_COLOR, linewidth=4)
-        self.ax.axhline(1, color=str(float(CONST.AX_LINE_COLOR) + 0.1), linewidth=2, linestyle='dashed')
-        self.ax.tick_params(color=CONST.AX_TICK_COLOR, which='both', labelcolor=CONST.AX_TICK_COLOR,
-                            labelsize=CONST.AX_TICK_SIZE)
-        self.ax.set_xlabel("Diameter (nm)", color=CONST.LABEL_COLOR, size=CONST.LABEL_SIZE)
-        self.ax.set_ylabel("CCNC/SMPS", color=CONST.LABEL_COLOR, size=CONST.LABEL_SIZE)
-        self.ax.set_title("CCNC/SMPS over Dry Diameter", color=CONST.TITLE_COLOR, size=CONST.TITLE_SIZE)
-        self.ccn_cn_ratio_points, = self.ax.plot([0], [0], 'o',
-                                                 color=CONST.CCNC_SMPS_POINT_COLOR, mew=0.5,
-                                                 ms=9, label="CCNC/SMPS")
+        self.ax.grid()
+        self.ax.axhline(0)
+        self.ax.axvline(0)
+        self.ax.axhline(1,  linestyle='dashed')
+        self.ax.tick_params(which='both')
+        self.ax.set_xlabel("Diameter (nm)")
+        self.ax.set_ylabel("CCNC/SMPS")
+        self.ax.set_title("CCNC/SMPS over Dry Diameter")
+        self.ccn_cn_ratio_points, = self.ax.plot([0], [0], 'o',label="CCNC/SMPS")
         handles, labels = self.ax.get_legend_handles_labels()
-        legend = self.ax.legend(handles, labels, loc=5, fontsize=CONST.LEGEND_FONT_SIZE)
-        legend.get_frame().set_alpha(0.3)
+        legend = self.ax.legend(handles, labels)
 
     def update_graph(self, a_scan):
         # extract the data
@@ -146,27 +139,22 @@ class RatioOverDiameterGraph(FigureCanvas):
 
 class TemperatureGraph(FigureCanvas):
     def __init__(self, parent=None):
-        self.fig, self.ax = plt.subplots(facecolor=CONST.GRAPH_BACKGROUND_COLOR)
+        self.fig, self.ax = plt.subplots()
         # set up the figure and axes
-        self.fig, self.ax = plt.subplots(facecolor=CONST.GRAPH_BACKGROUND_COLOR)
         super(self.__class__, self).__init__(self.fig)
-        self.ax.set_autoscale_on(True)
         self.ax.set_frame_on(False)
-        self.ax.grid(color=CONST.GRID_COLOR)
-        self.ax.axhline(0, color=CONST.AX_LINE_COLOR, linewidth=4)
-        self.ax.axvline(0, color=CONST.AX_LINE_COLOR, linewidth=4)
-        self.ax.tick_params(color=CONST.AX_TICK_COLOR, which='both', labelcolor=CONST.AX_TICK_COLOR,
-                            labelsize=CONST.AX_TICK_SIZE)
-        self.ax.set_xlabel("Scan time(s)", color=CONST.LABEL_COLOR, size=CONST.LABEL_SIZE)
-        self.ax.set_ylabel("Temperature", color=CONST.LABEL_COLOR, size=CONST.LABEL_SIZE)
-        self.ax.set_title("Temperature over scan time", color=CONST.TITLE_COLOR, size=CONST.TITLE_SIZE)
+        self.ax.axhline(0,)
+        self.ax.axvline(0)
+        self.ax.tick_params(which='both')
+        self.ax.set_xlabel("Scan time(s)")
+        self.ax.set_ylabel("Temperature")
+        self.ax.set_title("Temperature over scan time")
         # set up empty data lines
-        self.t1_line, = self.ax.plot([0], [0], linewidth=5, color=CONST.T1_LINE_COLOR, label="T1")
-        self.t2_line, = self.ax.plot([0], [0], linewidth=5, color=CONST.T2_LINE_COLOR, label="T2")
-        self.t3_line, = self.ax.plot([0], [0], linewidth=5, color=CONST.T3_LINE_COLOR, label="T3")
+        self.t1_line, = self.ax.plot([0], [0], label="T1")
+        self.t2_line, = self.ax.plot([0], [0], label="T2")
+        self.t3_line, = self.ax.plot([0], [0], label="T3")
         handles, labels = self.ax.get_legend_handles_labels()
-        legend = self.ax.legend(handles, labels, fontsize=CONST.LEGEND_FONT_SIZE)
-        legend.get_frame().set_alpha(0.3)
+        legend = self.ax.legend(handles, labels)
 
     def update_graph(self, a_scan):
         """
